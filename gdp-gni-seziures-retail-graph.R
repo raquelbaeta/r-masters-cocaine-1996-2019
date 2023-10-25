21bc2cfea2f5fd3d93c29aaa1db828c72b0f52dd
Raquel Baeta <99738835+raquelbaeta@users.noreply.github.com>
2023-10-25 12:59:49 +0200

    Update and rename gdp_gni_seziures_graph.R to gdp-gni-seziures-retail-graph.R

--- gdp-gni-seziures-retail-graph.R
+++ gdp-gni-seziures-retail-graph.R
@@ -1,7 +1,6 @@
 # Start
 
-# Thesis title: The Role of International Commitments in Combating the Illicit 
-# Distribution of Cocaine.
+# Thesis title: The Role of International Commitments in Combating the Illicit Distribution of Cocaine.
 # Author: Raquel Baeta
 
 # Set the working directory
@@ -12,14 +11,12 @@ library(ggplot2)
 library(ggpubr)
 library(stargazer)
 
-# ANOVA test: This test compares the means of a continuous response variable 
-# across different levels of one or more categorical predictor variables.
+# ANOVA test: This test compares the means of a continuous response variable across different levels of one or more categorical predictor variables.
 anova_gdp_log_euro <- aov(seizure_log ~ retail + gdp_euro_log, data = complete_data)
 summary(anova_gdp_log_euro)
 
 # Create a scatter plot with GDP, retail prices and cocaine seizures
-gdp_retail_seizure <- ggplot(
-  complete_data, aes(x = gdp_euro_log, y = retail, color = seizure_log)) +
+gdp_retail_seizure <- ggplot(complete_data, aes(x = gdp_euro_log, y = retail, color = seizure_log)) +
   geom_point() +
   labs(x = "Gross Domestic Product (in Euros)", 
        y = "Retail Price (in Euros)", 
@@ -34,14 +31,12 @@ gdp_retail_seizure <- ggplot(
 # Then, use ggsave() to the plot to a file
 ggsave("gdp_retail_seizure.png", plot = gdp_retail_seizure)
 
-# ANOVA test: This test compares the means of a continuous response variable 
-# across different levels of one or more categorical predictor variables.
+# ANOVA test: This test compares the means of a continuous response variable across different levels of one or more categorical predictor variables.
 gnicap_log <- aov(seizure_log ~ retail + gnicap_log, data = complete_data)
 summary(gnicap_log)
 
 # Create a scatter plot with GDP, retail prices and cocaine seizures
-gni_retail_seizure <- ggplot(
-  complete_data, aes(x = gnicap_log, y = retail, color = seizure_log)) +
+gni_retail_seizure <- ggplot(complete_data, aes(x = gnicap_log, y = retail, color = seizure_log)) +
   geom_point() +
   labs(x = "Gross National Income (in Euros)", 
        y = "Retail Price (in Euros)", 
@@ -57,9 +52,8 @@ gni_retail_seizure <- ggplot(
 ggsave("gni_retail_seizure.png", plot = gni_retail_seizure)
 
 # ggarange() can arrange multiple ggplots 
-gdp_gni_retail_seizure <- ggarrange(
-  gdp_retail_seizure, gni_retail_seizure + 
-    rremove("x.text"), labels = c("A", "B"), ncol = 1, nrow = 2)
+gdp_gni_retail_seizure <- ggarrange(gdp_retail_seizure, gni_retail_seizure + 
+                                    rremove("x.text"), labels = c("A", "B"), ncol = 1, nrow = 2)
 
 # Save the plots to a file
 ggsave("gdp_gni_retail_seizure.png", width = 11, height = 13, dpi = 500)
