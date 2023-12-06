e4a51081a6b3e750904c6cfea1a36a4d6fd0dfc6
Raquel Baeta <99738835+raquelbaeta@users.noreply.github.com>
2023-12-06 16:04:24 +0200

    Update cleaning_statelist.R

--- cleaning_statelist.R
+++ cleaning_statelist.R
@@ -1,6 +1,6 @@
 # Start 
 
-# Install necessary libraries if not already installed
+# Install libraries
 install.packages("readxl")
 install.packages("tidyverse")
 
@@ -20,21 +20,16 @@ statelist <- merge(cow_statelist, treaty, by = c("country", "id"), all.x = TRUE)
 
 # Replace NA with 0 in the specified columns
 columns_to_replace <- c("UN1961", "UN1971", "UN1988")
-statelist[columns_to_replace][is.na(statelist[columns_to_replace])] <- 0
-
-# Convert non-zero values to 1
-statelist[columns_to_replace] <- +(statelist[columns_to_replace] != 0)
-print(statelist)
+statelist[columns_to_replace][is.na(statelist[columns_to_replace])] <- 0  # Replace NAs with 0s to indicate lack of commitment
+statelist[columns_to_replace] <- (statelist[columns_to_replace] != 0)     # Convert non-zero values to 1
+print(statelist) # check
 
 # Remove duplicate rows
 statelist <- statelist[!duplicated(statelist), ]
-print(statelist)
+print(statelist) # check
 
 # Save as a .csv
-write.csv(
-  statelist, 
-  file = "~/Desktop/working-sessions/cleaning_data/statelist.csv", 
-  row.names = FALSE)
+write.csv(statelist, file = "~/Desktop/working-sessions/cleaning_data/statelist.csv", row.names = FALSE)
 
 # Save as a .rds
 saveRDS(statelist, "~/Desktop/working-sessions/statelist.rds")
