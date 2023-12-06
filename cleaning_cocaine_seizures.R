e6ae72903103535358c7dc26d61ee1b56d1dfc60
Raquel Baeta <99738835+raquelbaeta@users.noreply.github.com>
2023-12-06 15:58:04 +0200

    Update and rename cleaning_seizures.R to cleaning_cocaine_seizures.R

--- cleaning_cocaine_seizures.R
+++ cleaning_cocaine_seizures.R
@@ -39,7 +39,6 @@ cocaine_data_long <- cocaine_data_filtered %>%
   pivot_longer(cols = "1990":"2019", 
                names_to = "year", 
                values_to = "seizures")
-
 print(cocaine_data_long) # Viewing the result
 summary(cocaine_data_long) # check seizure observations
 
@@ -50,19 +49,15 @@ rm(cocaine_data, filled_data, cocaine_data_filtered)
 cocaine_data_long <- cocaine_data_long %>%
   mutate(merged_drug = ifelse(!is.na(drug_group), drug_group, drug))
 
-# Now you can remove the original drug-type and drug columns if needed
+# Now you can remove the original drug type and drug columns if needed
 cocaine_data_long <- cocaine_data_long %>%
   select(-drug_group, -drug)
 
 # data frame is named cocaine_data
-cocaine_data_long <- mutate(cocaine_data_long, 
-                            seizures = ifelse(is.na(seizures), 0, seizures))
+cocaine_data_long <- mutate(cocaine_data_long, seizures = ifelse(is.na(seizures), 0, seizures))
 
 # Save as a .csv
-write.csv(
-  cocaine_data_long, 
-  file = "~/Desktop/working-sessions/cleaning_data/cocaine_data_long.csv", 
-  row.names = FALSE)
+write.csv(cocaine_data_long, file = "~/Desktop/working-sessions/cleaning_data/cocaine_data_long.csv", row.names = FALSE)
 
 # Save as a .rds
 saveRDS(cocaine_data_long, "~/Desktop/working-sessions/cleaning_data.rds")
