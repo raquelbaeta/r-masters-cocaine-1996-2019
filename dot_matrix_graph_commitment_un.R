e345d43b46735431dc0744d2c100905461838482
Raquel Baeta <raquelcbaeta.sa@gmail.com>
2024-03-19 17:06:43 +0200

    Add files via upload

---
+++ dot_matrix_graph_commitment_un.R
@@ -0,0 +1,67 @@
+# Start
+
+# title: 'Plotting a Dot Matrix for commitment' 
+# author: 'Raquel Baeta'
+# date: '2023-07-12'
+
+# working directory
+setwd("~/Desktop/working-sessions")
+
+# install packages
+install.packages(c("reshape2", "wesanderson"))
+
+# load packages
+library(c(reshape2, wesanderson))
+
+# Dot matrix plot
+df <- data.frame(
+  ctry = c("AUT", "BEL", "DNK", "FIN", "DEU", "GRC", "IRL", "LUX", "NLD", "NOR",
+           "PRT", "ESP", "SWE", "CHE"),
+  UN_1961 = c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+              TRUE, TRUE, TRUE),
+  UN_1971 = c(FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, 
+              FALSE, FALSE, TRUE, FALSE),
+  UN_1988 = c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+              TRUE, TRUE, TRUE))
+
+# reduce the size of plotting symbols for better spacing
+symbol_size <- 3.5
+
+# remove the first year
+df_matrix <- as.matrix(df[, -1])  # only the data
+newdata <- reshape2::melt(df_matrix)
+newdata  # reshaped
+
+# labels
+ctry_labs <- df$ctry
+treaty_labs <- c("UN (1961)", "UN (1971)", "UN (1988)")
+
+# adjust margins and spacing
+par(mar=c(2, 2, 2, 2) + 0.1, xpd=TRUE)
+
+# specify the path to save the plot in the working directory as a PNG
+pdf("dot_matrix.pdf", width=6, height=6) # adjust figure dimensions
+
+# load 'GrandBudapest2' palette
+grandbudapest2 <- wes_palette("GrandBudapest2")
+
+with(newdata,
+     plot(as.numeric(Var2), rev(Var1),
+          pch=c(1, 19)[value+1],
+          col=grandbudapest2[value+1], # color dots based on value
+          cex=symbol_size,
+          axes=FALSE,
+          xlab="",
+          ylab="")
+)
+
+mtext(rev(ctry_labs), side=2,line=1 , at=1:length(ctry_labs), las=1)
+mtext(treaty_labs , side=3,line=2 , at=1:length(treaty_labs))
+
+# add a title
+mtext("State Commitment to United Nations Conventions", side=1,line=2)
+
+# save the plot
+dev.off()
+
+# End
